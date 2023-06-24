527bf9f06b87f9221eb814bc45a35771c35dad1b
Ciprian Florescu <ciprian.florescu@softwire.com>
2023-06-24 17:10:53 +0300

    Initial repo - bolierplate

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for CSF Home Component integration."""
---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for CSF Home Component tests."""
+from custom_components.csf_home_component.const import (
+    CONF_PASSWORD,
+)
+from custom_components.csf_home_component.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
